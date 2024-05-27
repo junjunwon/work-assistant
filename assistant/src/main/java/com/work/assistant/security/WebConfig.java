f586f4ccfdbb9f2f68a561791c5aa4c5335b532c
원준호 <jhwon@daou.co.kr>
2024-05-27 13:06:54 +0900

    Feat: WebConfig 적용 (CORS)

---
+++ assistant/src/main/java/com/work/assistant/security/WebConfig.java
@@ -0,0 +1,18 @@
+package com.work.assistant.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000") // Vue 애플리케이션이 실행되는 도메인
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
