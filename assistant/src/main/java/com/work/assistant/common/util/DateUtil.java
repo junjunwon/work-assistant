180ddcb1aa75566962d0c56cbbe994a534cdd84b
원준호 <jhwon@daou.co.kr>
2024-05-27 13:07:04 +0900

    feat: DateUtil 추가

---
+++ assistant/src/main/java/com/work/assistant/common/util/DateUtil.java
@@ -0,0 +1,22 @@
+package com.work.assistant.common.util;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DateUtil {
+
+    public static LocalDate convertToLocalDate(Date dateToConvert) {
+        return Instant.ofEpochMilli(dateToConvert.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDate();
+    }
+
+    public static LocalDateTime convertToLocalDateTime(Date dateToConvert) {
+        return Instant.ofEpochMilli(dateToConvert.getTime())
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime();
+    }
+}
