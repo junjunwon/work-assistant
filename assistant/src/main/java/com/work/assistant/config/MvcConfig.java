74a900a37db34c763f799132562db53cf6be46a8
junjunwon <45681336+junjunwon@users.noreply.github.com>
2024-10-03 00:49:41 +0900

    feat: interceptor를 활용한 logger access log 적용
    reference : https://www.baeldung.com/spring-mvc-handlerinterceptor

---
+++ assistant/src/main/java/com/work/assistant/config/MvcConfig.java
@@ -0,0 +1,23 @@
+﻿package com.work.assistant.config;
+
+import com.work.assistant.common.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan
+public class MvcConfig implements WebMvcConfigurer {
+
+    public MvcConfig() {
+        super();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+    }
+}
