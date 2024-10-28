82cd1c225b5d5f675ae863c8a9ff7a94f2912479
junjunwon <45681336+junjunwon@users.noreply.github.com>
2024-10-28 20:40:32 +0900

    feat: 랜딩 페이지 문구 변경

--- work-assistant-front/src/views/HomePage.vue
+++ work-assistant-front/src/views/HomePage.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="container">
       <div class="box">
-        <p class="title">면접만큼은 커밋 취소가 안 됩니다. <br />지금부터 리허설 시작!</p>
-        <p class="subtitle">당신의 면접을 시뮬레이션 해보세요 🤓</p>
+        <p class="title">면접만큼은 커밋 취소가 안 됩니다. <br />지금부터 모의면접 시작!</p>
+        <p class="subtitle">개발자 면접 많이 어려우시죠? <br />면접 영상을 녹화하고 스스로 피드백해보세요 🤓</p>
         <button class="button-text" @click="goToInterviewSelection">START</button>
         <div class="image-box">
           <img style="width:400px; height: 400px;"
