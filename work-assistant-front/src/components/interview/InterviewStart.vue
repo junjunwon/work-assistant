9e589e28171b161c764df79bae65a4058a8072a2
BAE\baedd <yeonjungbae@daou.co.kr>
2024-10-28 18:45:32 +0900

    면접 시작 전 guide추가

--- work-assistant-front/src/components/interview/InterviewStart.vue
+++ work-assistant-front/src/components/interview/InterviewStart.vue
@@ -6,10 +6,19 @@
         <NumberComboBox :maxNumber="30" v-model="selectedNumber" />
         개로 면접이 진행됩니다.
       </h2>
-      <p class="box">면접 준비가 되었으면 "YES"를 눌러주세요!</p>
+      <p class="box">
+        면접 시작 시 마이크와 카메라가 활성화되며,<br>
+        <span style="color: #894cff;">녹화된 영상은 종료 즉시 안전하게 삭제</span>됩니다.<br>
+        안심하고 이용하세요! 😌
+      </p>
+      <p class="box">
+        이제, 면접을 시작해 볼까요?
+        <br>
+        준비가 되었으면 "START"를 눌러주세요!
+      </p>
 
       <div class="box">
-        <button class="button-text" @click="startInterview">YES</button>
+        <button class="button-text" @click="startInterview">START</button>
       </div>
     </div>
 
